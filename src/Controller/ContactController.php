84169f9f7265e24a806e518fc55ba61f510ad13d
xshakhmatov <xshakhmatov@gmail.com>
2023-07-22 23:50:31 +0200

    adding delete and pagination commit

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,15 +13,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/', name: 'app_contact')]
+    public function index(PaginatorInterface $paginator, Request $request, ManagerRegistry $doctrine): Response
     {
         $contactRepository = $doctrine->getRepository(Contact::class);
         $contacts = $contactRepository->findAll();
 
+        $em = $doctrine->getManager();
+        $query = $em->getRepository(Contact::class)->createQueryBuilder('e')->getQuery();
+        $pagination = $paginator->paginate(
+            $query, // ваш запрос
+            $request->query->getInt('page', 1), // параметр передающий текущую страницу
+            10 // количество элементов на странице
+        );
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
             'contacts' => $contacts,
+            'pagination' => $pagination,
         ]);
     }
 
@@ -51,37 +61,34 @@ class ContactController extends AbstractController
         ]);
     }
 
-    #[Route('/contact/{lastName}/edit', name: 'contact_edit')]
-    public function edit(Contact $contact, Request $request, ManagerRegistry $doctrine): Response
+    #[Route('/contact/edit/{firstName}', name: 'contact_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, string $firstName, ManagerRegistry $doctrine): Response
     {
-        // Проверка наличия контакта с указанным ID
+        $contactRepository = $doctrine->getRepository(Contact::class);
+        $contact = $contactRepository->findOneBy(['firstName' => $firstName]);
+    
         if (!$contact) {
             throw $this->createNotFoundException('Контакт не найден');
         }
-
-        // Создание формы существующего контакта
+    
         $form = $this->createForm(ContactFormType::class, $contact);
-
-        // Обработка отправки формы
+    
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // Сохранение изменений контакта в базу данных
             $entityManager = $doctrine->getManager();
             $entityManager->persist($contact);
             $entityManager->flush();
-
-            // Редирект на страницу с контактами
-            return $this->redirectToRoute('contact_index');
+    
+            return $this->redirectToRoute('app_contact');
         }
-
-        // Отображение формы
+    
         return $this->render('contact/edit.html.twig', [
             'form' => $form->createView(),
             'contact' => $contact,
         ]);
     }
 
-    #[Route('/contact/{lastName}', name: 'contact_delete')]
+    #[Route('/contact/delete/{firstName}', name: 'contact_delete')]
     public function delete(Contact $contact, ManagerRegistry $doctrine): Response
     {
         // Проверка наличия контакта с указанным ID
